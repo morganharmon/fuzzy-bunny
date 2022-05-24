a3b12ff31750b722695afaba1678a5e630a11780
Morgan Harmon <morganharmon@gmail.com>
2022-05-23 17:23:07 -0700

    Delete bunny on drag and drop

--- families/families.js
+++ families/families.js
@@ -1,4 +1,4 @@
-import { checkAuth, deleteBunny, getFamilies, logout } from '../fetch-utils.js';
+import { checkAuth, createBunny, deleteBunny, getFamilies, logout } from '../fetch-utils.js';
 
 checkAuth();
 
@@ -19,16 +19,21 @@ async function displayFamilies() {
         const h3 = document.createElement('h3');
         const div2 = document.createElement('div');
         div.classList.add('family');
+        // give each div an id equal to its supabase id
+        div.id = family.id;
         div2.classList.add('bunnies');
         h3.textContent = family.name;
         div.append(h3, div2);
         // need to preventDefault on all drag events, as well as setData on the id on dragstart and getData on drop so it "remembers" which item has moved
-        div.addEventListener('drop', (e) => {
+        div.addEventListener('drop', async (e) => {
             e.preventDefault();
             const id = e.dataTransfer.getData('text/plain');
             const dropBunny = document.getElementById(id);
             e.target.append(dropBunny);
             dropBunny.classList.remove('hide');
+            
+            await deleteBunny(id);
+            // await createBunny({ name: id, family_id: e.target.id });
         });
         div.addEventListener('dragenter', (e) => {
             e.preventDefault();
@@ -46,7 +51,7 @@ async function displayFamilies() {
             });
             // set to draggable and setData. Hide the object the instant it is clicked to drag, but setTimeout to 0 so that you can "pick up" the still visible object and drag it around, and the left behind object is then immediately set to hidden
             divBun.draggable = true;
-            divBun.id = bunny.name;
+            divBun.id = bunny.id;
             divBun.addEventListener('dragstart', (e) => {
                 e.dataTransfer.setData('text/plain', e.target.id);
                 setTimeout(() => {
